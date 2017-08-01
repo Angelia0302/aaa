1c683c6728992d18d5282d4f905cd134ebfd40bb
cuixiying <cuixiying@51jianji.com>
2017-08-01 20:29:57 +0800

    dongtai

--- jianji/src/components/page/msg.vue
+++ jianji/src/components/page/msg.vue
@@ -17,9 +17,9 @@
   				<td colspan="0" border="1">{{main.id}}</td>
   				<td colspan="0" border="1">
   					<span @click.stop="billClick(i, main.id)">开票</span>
-  					<span @click.stop="updateClick">修改</span>
-  					<span @click.stop="redClick">红票</span>
-  					<span @click.stop="delClick">删除</span>
+  					<span @click.stop="updateClick(i, main.id)">修改</span>
+  					<span @click.stop="redClick(i, main.id)">红票</span>
+  					<span @click.stop="delClick(i, main.id)">删除</span>
   				</td>
   				<td colspan="0" border="1">{{main.created_date}}</td>
   				<td colspan="0" border="1">{{main.last_modified_date}}</td>
@@ -38,71 +38,185 @@
   			</tr>
   		</tbody>
   	</table>
-  	<!-- 开票 -->
-  	<form class="from_p" width="500px" v-show='tickeInfoShow'>
-  		<div id="lab" class="ax_default label">
-        <div class="text">
-          <p><span>发票号</span></p>
-        </div>
+      <!-- 开票 -->
+    <form class="from_xiu"  v-show='tickeInfoShow'>
+      <div class="top_fa">
+      <p>
+        <label>
+          发票号
+        </label>
+        <input type="text" v-model="billData.invoiceNumber" placeholder="发票号">
+      </p>
+      <p>
+        <label>
+          重开关联票号
+        </label>
+        <input type="text" v-model="billData.输入后匹配检索效验" placeholder="输入后匹配检索效验" >
+      </p>
+        
       </div>
-  		<div class="ax_default text_field">
-        <input id="u_input" type="text" v-model="billData.invoiceNumber" placeholder="发票号" style="color: rgb(153, 153, 153);">
-      </div>
-      <div id="rigt" class="ax_default label">
-        <div class="text">
-          <p><span>重开关联号</span></p>
-        </div>
-      </div>
-  		<div class="text_fie">
-        <input id="input_b" type="text" v-model="billData.输入后匹配检索效验" placeholder="输入后匹配检索效验" style="color: rgb(153, 153, 153);">
-      </div>
-      <div class="ax_default label beizhu">
-        <p><span>备注</span></p>
-      </div>
-      <div class="ax_default text_area">
-        <textarea v-model="billData.remark" placeholder="备注" style="color: rgb(153, 153, 153);width:350px"></textarea>
+      <div class="textear">
+        <label>
+          备注
+        </label>
+        <textarea name="" v-model="billData.remark" placeholder="备注" id="textar" cols="50" rows="4"></textarea>
       </div>
       <div class="button" style="text-align: right;">
-        <button class="but1" @click="setTicket">确定</button>
-  	    <button class="but2" @click="visible2 = false">取消</button>   
-  		</div>
-      <div class="mssge">
-      	<label class="label_bot">
-      		公司名称
-      	</label>
-      	<input type="text" readonly v-model="selectItem.公司名称" style="color: rgb(153, 153, 153);width:350px;height:15px">
+      <button class="but1" @click="setTicket">确定</button>
+        <button class="but2" @click="visible2 = false">取消</button>
+        
       </div>
-      <div class="mssge">
-      	<label class="label_bot">
-      		纳税人号
-      	</label>
-      	<input type="text" readonly v-model="selectItem.纳税人号" style="color: rgb(153, 153, 153);width:350px;height:15px">
+      <div class="from_tom">
+        <div class="top_date">
+          <p class="date">
+        <label>
+          提交日期
+        </label>
+        <input type="date" v-model="selectItem.提交日期" style="color: rgb(153, 153, 153);"/>
+      </p>
+      <p class="date">
+        <label>
+          开票日期
+        </label>
+        <input type="date"  v-model="selectItem.开票日期" style="color: rgb(153, 153, 153)">
+      </p>
+        </div>
+      <div class="from_inp">
+        <label>
+          公司名称
+        </label>
+        <input type="text" v-model="selectItem.公司名称" placeholder="">  
+      </div><div class="from_inp">
+        <label>
+          纳税人号
+        </label>
+        <input type="text" v-model="selectItem.联系电话" placeholder="">  
       </div>
-      <div class="mssge">
-      	<label class="label_bot">
-      		开票金额
-      	</label>
-      	<input type="text" readonly v-model="selectItem.开票金额" style="color: rgb(153, 153, 153);width:350px;height:15px">
+      <div class="from_inp">
+        <label>
+          开票金额
+        </label>
+        <input type="text" v-model="selectItem.开票金额" placeholder="">  
       </div>
-      <div class="mssge">
-      	<label class="label_bot">
-      		邮箱
-      	</label>
-      	<input type="text" readonly v-model="selectItem.邮箱" style="color: rgb(153, 153, 153);width:350px;height:15px">
+      <div class="from_inp">
+        <label>
+          邮箱
+        </label>
+        <input type="text" v-model="selectItem.邮箱" placeholder="">  
       </div>
-      <div class="mssge">
-      	<label class="label_bot">
-      		联系人
-      	</label>
-      	<input type="text" readonly v-model="selectItem.联系人" style="color: rgb(153, 153, 153);width:350px;height:15px">
+      <div class="from_inp">
+        <label>
+          联系人
+        </label>
+        <input type="text" v-model="selectItem.联系人" placeholder="">  
       </div>
-      <div class="mssge">
-      	<label class="label_bot">
-      		联系电话
-      	</label>
-      	<input type="text" readonly v-model="selectItem.联系电话" style="color: rgb(153, 153, 153);width:350px;height:15px">
+      <div class="from_inp">
+        <label>
+          联系电话
+        </label>
+        <input type="text" readonly v-model="selectItem.联系电话" placeholder="">  
       </div>
-  	</form>
+      </div>
+    </form>
+    <!-- 修改 -->
+    <form class="from_xiu"  v-show='modifyShow'>
+      <div class="top_fa">
+      <p>
+        <label>
+          发票号
+        </label>
+        <input type="text" v-model="billData.invoiceNumber" placeholder="发票号">
+      </p>
+      <p>
+        <label>
+          重开关联票号
+        </label>
+        <input type="text" v-model="billData.输入后匹配检索效验" placeholder="输入后匹配检索效验" >
+      </p>
+        
+      </div>
+      <div class="textear">
+        <label>
+          备注
+        </label>
+        <textarea name="" v-model="billData.remark" placeholder="备注" id="textar" cols="50" rows="4"></textarea>
+      </div>
+      <div class="button" style="text-align: right;">
+      <button class="but1" @click="setTicket">确定</button>
+        <button class="but2" @click="visible2 = false">取消</button>
+        
+      </div>
+      <div class="from_tom">
+        <div class="top_date">
+          <p class="date">
+        <label>
+          提交日期
+        </label>
+        <input type="date" v-model="selectItem.提交日期" style="color: rgb(153, 153, 153);"/>
+      </p>
+      <p class="date">
+        <label>
+          开票日期
+        </label>
+        <input type="date"  v-model="selectItem.开票日期" style="color: rgb(153, 153, 153)">
+      </p>
+        </div>
+      <div class="from_inp">
+        <label>
+          公司名称
+        </label>
+        <input type="text" v-model="selectItem.公司名称" placeholder="">  
+      </div><div class="from_inp">
+        <label>
+          纳税人号
+        </label>
+        <input type="text" v-model="selectItem.联系电话" placeholder="">  
+      </div>
+      <div class="from_inp">
+        <label>
+          开票金额
+        </label>
+        <input type="text" v-model="selectItem.开票金额" placeholder="">  
+      </div>
+      <div class="from_inp">
+        <label>
+          邮箱
+        </label>
+        <input type="text" v-model="selectItem.邮箱" placeholder="">  
+      </div>
+      <div class="from_inp">
+        <label>
+          联系人
+        </label>
+        <input type="text" v-model="selectItem.联系人" placeholder="">  
+      </div>
+      <div class="from_inp">
+        <label>
+          联系电话
+        </label>
+        <input type="text" readonly v-model="selectItem.联系电话" placeholder="">  
+      </div>
+      </div>
+    </form>
+    <!-- 红票 -->
+    <div class="red_trcked" v-show="redShow">
+        <p>
+          <label>
+            红票发票号
+          </label>
+          <input type="text" v-model="redNumber.红票发票号">
+        </p>
+        <p>
+          <label>
+            原发票号
+          </label>
+          <input type="text" v-model="redNumber.原发票号">
+        </p>
+        <div class="button" style="text-align: center;">
+        <button class="but1" @click="visible2 = false">确定</button>
+        <button class="but2" @click="visible2 = false">取消</button>        
+      </div>
+    </div>
     <!-- 删除弹框 -->
     <div class="delt" v-show='delShow'>
     	<p style="color:red">确定删除吗？</p>
@@ -129,8 +243,12 @@ export default {
        flage:false,
        // 开票弹框
        tickeInfoShow: false,
+       // 修改弹框
+       modifyShow: false,
        // 删除弹框
        delShow: false,
+       // 红票弹框
+       redTrcked:false,
        // 选中的数据
        selectItem: {},
        // 填写开票的信息
@@ -158,7 +276,6 @@ export default {
     },
   
     mounted() {
-
     	/*axios({
 	    	url: 'http://192.168.8.177:4210/api/invoices',
 	    	menthod: 'GET',
@@ -169,14 +286,11 @@ export default {
     		console.log(res);
     		this.items = res.data;
     	});*/
-    	//
     	// 用js模拟后端数据格式，
     	this.items = mockData;
-    	
     	http.get('/api/invoices').then((res) => {
     		// 请求接口成功回调函数
     		// 正式数据在这里获取
-    		
     		console.log(res);
     		this.items = res.data;
     	}, (e) => {
@@ -184,32 +298,68 @@ export default {
     	});
     },
     methods: {
+      // 开票
       billClick (i) {
         if (!this.items) {return false;}
         this.selectItem = this.items[i];
         this.selectItem.index = i;
+         this.tickeInfoShow = true;
       },
       setTicket () {
-        http.put('/api/invoices' + this.selectItem.id,
-          data: {
-            id: this.selectItem.id,
-            invoiceNumber: this.billData.invoiceNumber,
-            remark: this.billData.remark
-          }
+        http.put('/api/invoices/' + this.selectItem.id,{
+        data:{
+                  id: this.selectItem.id,
+                  invoiceNumber: this.billData.invoiceNumber,
+                  remark: this.billData.remark
+                }
+              }
         ).then((res) => {
           // 请求接口成功回调函数
           // 正式数据在这里获取
           this.items.splice(this.selectItem.index, 1, res);
           this.selectItem = {};
           this.billData = {};
+           this.tickeInfoShow = false;
         }, (e) => {
           console.error(e);
         });
+      },
+      // 修改
+      updateClick (i) {
+        if (!this.items) {return false;}
+        this.selectItem = this.items[i];
+        this.selectItem.index = i;
+         this.modifyShow = true;
+      },
+      setTicket () {
+        http.put('/api/invoices/' + this.selectItem.id,{
+        data:{
+                  id: this.selectItem.id,
+                  invoiceNumber: this.billData.invoiceNumber,
+                  remark: this.billData.remark
+                }
+              }
+        ).then((res) => {
+          // 请求接口成功回调函数
+          // 正式数据在这里获取
+          this.items.splice(this.selectItem.index, 1, res);
+          this.selectItem = {};
+          this.billData = {};
+           this.modifyShow = false;
+        }, (e) => {
+          console.error(e);
+        });
+      },
+      // 红票
+      redClick(){
+        http.put()
       }
+ 
     }
 };
 </script>
 <style scoped lang='less'>
+@import '../../assets/css/msg.less';
 .msg{
 	position: relative;
 	left: 0;
@@ -223,275 +373,5 @@ export default {
 	width:100%;
 	overflow-x: auto;
 }
-.msg_top{
-	position: absolute;
-	top:30px;
-	left:180px;
-	width:100%;
-}
-	.input_tile{
-		 -webkit-appearance: none;
-	    -moz-appearance: none;
-	    appearance: none;
-	    background-color: #fff;
-	    background-image: none;
-	    box-sizing: border-box;
-	    color: #1f2d3d;
-	    font-size: 12px;
-	    height: 30px;
-	    line-height: 1;
-	    outline: 0;
-	    padding: 3px 10px;
-	    transition: border-color .2s cubic-bezier(.645,.045,.355,1);
 
-	  
-	}
-	.el-button el-button--primary{
-	display: inline-block;
-    line-height: 1;
-    white-space: nowrap;
-    cursor: pointer;
-    background: #fff;
-    border: 1px solid #c4c4c4; 
-    color: #1f2d3d;
-    margin: 0; 
-    color: #fff;
-    background-color: #20a0ff;
-    border-color: #20a0ff;
-    border-radius: 4px;
-	}
-	.table_th{
-		border-right: 1px solid #ccc; 
-		margin-top: 15px;
-		white-space:nowrap;
-	}
-	.table_th .cell{
-		font-size: 14px;
-		color:#fff;
-		border-left: 1px solid #ccc;
-		background: #20a0ff;
-		padding-left: 15px;
-		padding-right: 15px;
-		height:30px;
-	}
-	.table_th td{
-		font-size:14px; 
-		height:40px;
-		border-left: 1px solid #ccc;
-		border-bottom: 1px solid #ccc;
-		text-align: center;
-	}
-	.label_width{
-		width:100px;
-		border: 1px solid #000;
-		display: inline-block;
-		text-align: right;
-    vertical-align: middle;
-    float: left;
-    font-size: 14px;
-    color: #48576a;
-    line-height: 1;
-    padding: 5px 6px 5px 0;
-    box-sizing: border-box;
-	}
-	.input_inner{
-	-webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-    background-color: #fff;
-    background-image: none;
-    border: 1px solid #999;
-    box-sizing: border-box;
-    color: #1f2d3d;
-    font-size: inherit;
-    height: 25px;
-    line-height: 1;
-    width:150px;
-    outline: 0;
-    padding-right: 20px;
-    transition: border-color .2s cubic-bezier(.645,.045,.355,1);
-}
-.delt{
-	width: 295px;
-	height: 172px;
-	border: 1px solid #000;
-	position: relative;
-	top:20%;
-	left:40%;
-	p{
-		text-align: center;
-		color:red;
-	}
-	.button{
-		margin-top:70px;
-		margin-right: 20px;
-		.but1,.but2{
-	display: inline-block;
-    line-height: 1;
-    white-space: nowrap;
-    cursor: pointer;
-    border: 1px solid #c4c4c4;
-    color: #1f2d3d;
-    margin: 0;
-    padding: 8px 10px;
-     color: #fff;
-    background-color: #20a0ff;
-    border:none;
-    outline: none;
-
-		}
-		.but2{
-   			background: #999;
-		}
-	}
-}
-.from_p{
-	    border-width: 0px;
-    position: absolute;
-    left: 40px;
-    top: 50px;
-    width: 484px;
-    height: 456px;
-    background: inherit;
-    background-color: rgba(255, 255, 255, 1);
-    box-sizing: border-box;
-    border-width: 1px;
-    border-style: solid;
-    border-color: rgba(121, 121, 121, 1);
-    border-radius: 0px;
-    -moz-box-shadow: none;
-    -webkit-box-shadow: none;
-    box-shadow: none;
-    .ax_default{
-    font-weight: 400;
-    font-style: normal;
-    font-size: 13px;
-    color: #333333;
-    line-height: normal;
-    }
-    .text_field{
-   	border-width: 0px;
-    position: absolute;
-    left: 74px;
-    top: 20px;
-    width: 132px;
-    height: 25px;
-   
-    #u_input{
-   /*  position: absolute;
-   left: 0px;
-   top: 0px; */
-    width: 132px;
-    height: 25px;
-    box-sizing: border-box;
-    font-weight: 400;
-    font-style: normal;
-    font-size: 13px;
-    text-decoration: none;
-    color: #000000;
-    text-align: left;
-    }
-
-   }
-    .text_fie{
-   	border-width: 0px;
-   position: relative;
-   left: 80px;
-   top: 15px;
-   width: 132px;
-   height: 25px;
-    #input_b{
-    position: absolute;
-    left: 220px;
-    top: 0px;
-    width: 132px;
-    height: 25px;
-    padding: 0;
-    box-sizing: border-box;
-    font-weight: 400;
-    font-style: normal;
-    font-size: 13px;
-    text-decoration: none;
-    color: #000000;
-    } 
-   
-    }
-    .label{
-    	    font-size: 14px;
-    		text-align: left;
-    }
-    .beizhu{
-    	    border-width: 0px;
-    position: absolute;
-    left: 21px;
-    top: 50px;
-    width: 29px;
-    height: 20px;
-    }
-    #lab{
-   	border-width: 0px;
-    position: absolute;
-    left: 21px;
-    top: 5px;
-    width: 43px;
-    height: 20px;
-    }
-    #rigt{
-    	    border-width: 0px;
-    position: absolute;
-    left: 220px;
-    top: 5px;
-    width: 85px;
-    white-space: nowrap;
-    }
-    .text_area{
-   	border-width: 0px;
-    position: absolute;
-    left: 74px;
-    top: 67px;
-    width: 600px;
-    height: 45px;
-    outline: none;
-
-    }
-    .button{
-    	width:483px;
-		margin-top:130px;
-		border-bottom: 1px solid #000;
-
-		.but1,.but2{
-	display: inline-block;
-    line-height: 1;
-    white-space: nowrap;
-    cursor: pointer;
-    border: 1px solid #c4c4c4;
-    color: #1f2d3d;
-    margin: 0;
-    padding: 8px 10px;
-     color: #fff;
-    background-color: #20a0ff;
-    border:none;
-    outline: none;
-    border-radius: 5px;
-    margin-right: 20px;
-    margin-bottom: 15px;
-
-		}
-		.but2{
-   			background: #999;
-		}
-	}
-	.mssge{
-		margin-top: 20px;
-		.label_bot{
-		font-size: 14px;
-    	width:200px;
-    	text-align: center;
-	}
-	input{
-		width:300px;
-	}
-	}
-	
-}
 </style>
